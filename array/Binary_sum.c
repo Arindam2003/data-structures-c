97007bad14ec8df62657b35736171c8fb6e5df0f
Arindam2003 <dindaarindam000@gmail.com>
2024-09-26 23:44:57 +0530

    Add Binary

---
+++ array/Binary_sum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    // int *p;
+    int a1[5],a2[5],s[5];
+    
+    a1[0]=0;
+    int i;
+    printf("Enter First binary numebr: ");
+    for (i=1;i<5;i++)
+    {
+        scanf("%d",&a1[i]);
+    }
+    printf("Enter Second binary numebr: ");
+    a2[0]=0;
+    for (i=1;i<5;i++)
+    {
+        scanf("%d",&a2[i]);
+    }
+    
+    
+}
